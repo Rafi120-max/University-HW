80ac651591bac2fd9b3f9935fc5c6db92b840eed
Rafi Ahmed <84710018+oyasick@users.noreply.github.com>
2022-06-25 13:18:16 +0600

    Add files via upload

---
+++ Explain conditonal Operatorc.c
@@ -0,0 +1,16 @@
+Expalin condiitonal operator with proper example.
+Ans:
+The conditiona operator is also known as a ternary operator.
+The conditional are the decision -making statement
+which depends upon the output of the expression.It is represnted by two symbols,i.e '?':
+	
+As conditional operator woeks on three operands,so it is also known as the ternary operator.
+
+The behavior of the conditional operator is similar to the 'if-else' statementas 
+if-else' statement is also a decision making statement.
+Syntex of conditional operator.
+Exp1?exp2:exp3;
+Example.
+(marks>40)?printf("Passed"):printf(failed);
+	
+		
